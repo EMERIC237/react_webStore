0e3bfce0976b37e39438faf50f482a1771998b12
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2022-12-17 06:40:41 -0600

    Project: pembo-shop(nextJS + Sanity) Required packages installed and Sanity account setup

---
+++ pembo-shop/pemboshop/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'pemboShop',
+
+  projectId: '9m1hjh3n',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
